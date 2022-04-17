c846772c8f50ee24f1bae5c6b41ec40ec8b4731e
Wariashi <wariashi@weizengaming.de>
2022-04-17 15:25:36 +0200

    Add some Javadoc

--- src/main/java/de/wariashi/bomberbew/model/Map.java
+++ src/main/java/de/wariashi/bomberbew/model/Map.java
@@ -1,10 +1,24 @@
 package de.wariashi.bomberbew.model;
 
+/**
+ * The map is a two-dimensional array of tiles which consist of different
+ * {@link Material materials}.
+ */
 public class Map {
 	private int width;
 	private int height;
 	private Material[][] materials;
 
+	/**
+	 * Creates a new {@link Map}.
+	 * 
+	 * @param width        the width of the map in tiles
+	 * @param height       the height of the map in tiles
+	 * @param brickDensity a value between 0.0 and 1.0 which describes the
+	 *                     percentage of the map that should be filled with
+	 *                     {@link Material#BRICK bricks}
+	 * @throws NegativeArraySizeException if the width or height is negative
+	 */
 	public Map(int width, int height, double brickDensity) {
 		this.width = width;
 		this.height = height;
@@ -15,10 +29,24 @@ public class Map {
 		clearCorners();
 	}
 
+	/**
+	 * Returns the height of the map in tiles.
+	 * 
+	 * @return the height of the map
+	 */
 	public int getHeight() {
 		return height;
 	}
 
+	/**
+	 * Returns the {@link Material} of the tile at the given coordinate. If the
+	 * requested coordinates are outside of the map, {@link Material#CONCRETE} will
+	 * be returned.
+	 * 
+	 * @param x the x coordinate of the tile
+	 * @param y the y coordinate of the tile
+	 * @return the {@link Material} of the tile
+	 */
 	public Material getMaterial(int x, int y) {
 		if (x < 0 || width <= x || y < 0 || height <= y) {
 			return Material.CONCRETE;
@@ -26,10 +54,22 @@ public class Map {
 		return materials[x][y];
 	}
 
+	/**
+	 * Returns the width of the map in tiles.
+	 * 
+	 * @return the width of the map
+	 */
 	public int getWidth() {
 		return width;
 	}
 
+	/**
+	 * Adds {@link Material#BRICK bricks} at random positions to the map.
+	 * 
+	 * @param brickDensity a value between 0.0 and 1.0 which describes the
+	 *                     percentage of the map that should be filled with
+	 *                     {@link Material#BRICK bricks}
+	 */
 	private void addBricks(double brickDensity) {
 		for (var y = 0; y < height; y++) {
 			for (var x = 0; x < width; x++) {
@@ -40,6 +80,9 @@ public class Map {
 		}
 	}
 
+	/**
+	 * Adds {@link Material#CONCRETE concrete} pillars to the map.
+	 */
 	private void addPillars() {
 		for (var y = 0; y < height; y++) {
 			for (var x = 0; x < width; x++) {
@@ -50,6 +93,10 @@ public class Map {
 		}
 	}
 
+	/**
+	 * Removes bricks and concrete from the corners of the map so that can
+	 * {@link Player players} spawn there.
+	 */
 	private void clearCorners() {
 		// top left
 		materials[0][0] = Material.EMPTY;
@@ -72,6 +119,11 @@ public class Map {
 		materials[width - 1][height - 1] = Material.EMPTY;
 	}
 
+	/**
+	 * Initializes the map with {@link Material#EMPTY empty} space.
+	 * 
+	 * @throws NegativeArraySizeException if the width or height is negative
+	 */
 	private void initializeMaterials() {
 		materials = new Material[width][height];
 		for (var y = 0; y < height; y++) {
--- src/main/java/de/wariashi/bomberbew/model/Material.java
+++ src/main/java/de/wariashi/bomberbew/model/Material.java
@@ -1,5 +1,21 @@
 package de.wariashi.bomberbew.model;
 
+/**
+ * Represents the state of a tile within the {@link Map}.
+ */
 public enum Material {
-	BRICK, CONCRETE, EMPTY;
+	/**
+	 * A tile that can be removed by a {@link Player} by placing a bomb next to it.
+	 */
+	BRICK,
+
+	/**
+	 * A tile that can not be removed by a {@link Player}.
+	 */
+	CONCRETE,
+
+	/**
+	 * An empty tile that is passable by a {@link Player}.
+	 */
+	EMPTY;
 }
