fcf2a588606fb3f6f5d7c58dd1af1cb0005af655
Wariashi <wariashi@weizengaming.de>
2022-04-23 13:57:39 +0200

    Add some Javadoc

--- src/main/java/de/wariashi/bomberbew/controller/Controller.java
+++ src/main/java/de/wariashi/bomberbew/controller/Controller.java
@@ -1,5 +1,21 @@
 package de.wariashi.bomberbew.controller;
 
+import de.wariashi.bomberbew.model.Game;
+import de.wariashi.bomberbew.model.Player;
+
+/**
+ * A {@link Controller} can be used to control a {@link Player}.
+ */
 public interface Controller {
+	/**
+	 * This method is called periodically to let the {@link Controller} decide what
+	 * to do next.
+	 * 
+	 * @param input a {@link ControllerInput} object containing some information
+	 *              about the current state of the {@link Game}
+	 * @return a {@link ControllerOutput} object which contains information about
+	 *         what the {@link Player} should do next. This value can never be
+	 *         <code>null</code>.
+	 */
 	public ControllerOutput update(ControllerInput input);
 }
--- src/main/java/de/wariashi/bomberbew/controller/ControllerOutput.java
+++ src/main/java/de/wariashi/bomberbew/controller/ControllerOutput.java
@@ -1,14 +1,35 @@
 package de.wariashi.bomberbew.controller;
 
 import de.wariashi.bomberbew.model.Direction;
+import de.wariashi.bomberbew.model.Game;
+import de.wariashi.bomberbew.model.Player;
 
+/**
+ * The {@link ControllerOutput} can be used by a {@link Controller} to tell the
+ * {@link Game} what the {@link Player} should do next.
+ */
 public class ControllerOutput {
 	private Direction direction = null;
 
+	/**
+	 * Returns the {@link Direction} where the {@link Player} should go next. This
+	 * can also be <code>null</code> to indicate that the {@link Player} should not
+	 * move at all.
+	 * 
+	 * @return the {@link Direction} where the {@link Player} should go next
+	 */
 	public Direction getDirection() {
 		return direction;
 	}
 
+	/**
+	 * Sets the {@link Direction} where the {@link Player} should go next. This can
+	 * also be <code>null</code> to indicate that the {@link Player} should not move
+	 * at all.
+	 * 
+	 * @param direction the {@link Direction} where the {@link Player} should go
+	 *                  next
+	 */
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
